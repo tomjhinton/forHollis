e6cd7064470501559506e1a2dc7a568eff96206e
Tom Hinton <tomjhinton@gmail.com>
2020-01-18 21:03:04 +0000

    setup

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import 'bulma'
+import './style.scss'
+
+var x = document.getElementById('demo')
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+getLocation()
+
+var video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
