5242a2abf95703d4802f0b6b81bb5c266e2f91e2
Tom Hinton <tomjhinton@gmail.com>
2020-01-20 18:11:09 +0000

    problems with webpack build

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import 'bulma'
 import './style.scss'
+const THREE = require('three')
+const AFRAME = import('aframe')
+import('aframe-extras')
 
+
+
+let lat, long
 var x = document.getElementById('demo')
 function getLocation() {
   if (navigator.geolocation) {
@@ -11,20 +17,49 @@ function getLocation() {
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
+  // x.innerHTML = "Latitude: " + position.coords.latitude +
+  // "<br>Longitude: " + position.coords.longitude;
+  lat = position.coords.latitude
+  long = position.coords.longitude
 }
 
 getLocation()
 
-var video = document.querySelector("#videoElement");
+window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
 
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function (stream) {
-      video.srcObject = stream;
-    })
-    .catch(function (err0r) {
-      console.log("Something went wrong!");
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Magnemite',
+            location: {
+                lat: lat,
+                lng: long,
+            }
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gltf-model', './assets/scene.gltf');
+        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '0.5 0.5 0.5');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
     });
 }
